f4d685fdef6507dd57dd7cf609f481cf937b2d19
yskgori0407@gmail.com <ec2-user@ip-172-31-2-192.ec2.internal>
2020-12-03 07:28:25 +0000

    課題3提出2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :index, :update]
   resources :books do
-    resources :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
 
