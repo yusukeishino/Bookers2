826316d07428c21e0ce61298c394baf5639a33a2
yskgori0407@gmail.com <ec2-user@ip-172-31-2-192.ec2.internal>
2020-12-07 07:15:45 +0000

    課題提出

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -4,4 +4,7 @@ class BookComment < ApplicationRecord
     belongs_to :user
     belongs_to :book
 
+    validates :comment, presence: true
+
+
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,6 +3,6 @@ class Favorite < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
-  validates_uniqueness_of :book_id, scope: :user_id
+
 
 end
